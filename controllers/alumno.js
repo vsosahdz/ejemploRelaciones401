a52e150d1bdb5aa71de783a6a6ec61ca5b82bec0
vsosahdz <vsosa@tec.mx>
2021-04-16 21:03:08 -0500

    Actualizacion Viernes 2

--- controllers/alumno.js
+++ controllers/alumno.js
@@ -12,3 +12,44 @@ exports.postAgregarAlumno = (req, res)=>{
             res.send("Hubo un error") //Cliente
         })
 }
+
+exports.getAlumnos = (req,res)=>{
+    //SELECT * from alumnos;
+    Alumno.findAll()
+        .then(alumnos=>{
+            console.log("Usuario:", alumnos);
+            res.send(alumnos)
+        })
+}
+
+exports.postEliminarAlumno = (req,res)=>{
+    //DELETE FROM alumno WHERE id=...
+    console.log(req.body)
+    Alumno.destroy({
+        where: {
+            id: req.body.id
+        }
+    }).then(()=>{
+        console.log("Alumno eliminado");
+        res.send("Alumno eliminado")
+    }).catch(error=>{
+        console.log(error);
+        res.send("Error en el proceso")
+    })    
+}
+
+exports.postActualizarAlumno = (req,res)=>{
+    //UPDATE alumno SET  WHERE id=...
+    console.log(req.body)
+    Alumno.update({nombre: req.body.nombre},{
+        where: {
+            id: req.body.id
+        }
+    }).then(()=>{
+        console.log("Alumno actualizado");
+        res.send("Alumno actualizado")
+    }).catch(error=>{
+        console.log(error);
+        res.send("Error en el proceso")
+    })    
+}
--- routes/alumno.js
+++ routes/alumno.js
@@ -2,5 +2,8 @@ const router = require("express").Router();
 const alumnoController = require('../controllers/alumno');
 
 router.post('/agregarAlumno',alumnoController.postAgregarAlumno);
+router.get('/verAlumnos',alumnoController.getAlumnos);
+router.post('/eliminarAlumno',alumnoController.postEliminarAlumno);
+router.post('/actualizarAlumno',alumnoController.postActualizarAlumno);
 
 module.exports = router;
